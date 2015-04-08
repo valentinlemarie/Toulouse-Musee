87fb4bec657d411c8208e504fe26ed159446c0c9
Marcfr <marcalquier81@gmail.com>
2015-04-08 15:00:11 +0200

    test unit musee

--- test/unit/musee/MuseeSpec.groovy
+++ test/unit/musee/MuseeSpec.groovy
@@ -11,22 +11,43 @@ import spock.lang.Unroll
 class MuseeSpec extends Specification {
 
     @Unroll
-    void "test la validite d'un musee valide"(String unNom, String desHoraires, String unTelephone, String unAccesMetro, String unAccesBus){
+    void "test la validite d'un musee valide"(String unAccesMetro, String unAccesBus){
         given: "un musee initialise correctement"
-        Musee musee = new Musee(nom: unNom, horairesOuverture: desHoraires, telephone: unTelephone, accesMetro: unAccesMetro, accesBus: unAccesBus)
+        Musee musee = new Musee(nom: "Musee", horairesOuverture: "lundi au vendredi de 10h a 20h", telephone: "0563214582", accesMetro: unAccesMetro, accesBus: unAccesBus)
 
         expect: "le musee est valide"
         musee.validate() == true
 
         where:
-        unNom       | desHoraires                                           | unTelephone   | unAccesMetro  | unAccesBus
-        "Musee"     | "lundi au vendredi de 10h a 20h"                      | "0563214582"  | "13, 17"      | "1, 2"
-        "Musee 2"   | "lundi au vendredi de 10h a 20h, samedi de 9h a 22h"  | "0548612458"  | "21, 48, 78"  | ""
-        "Musee 3"   | "lundi au vendredi de 10h a 21h"                      | "0548784154"  | ""            | "3, 5"
-        "Musee 4"   | "lundi au vendredi de 12h a 20h, samedi de 10h a 22h" | "0515218756"  | ""            | ""
-        "Musee 5"   | "lundi au vendredi de 10h a 20h"                      | "0531285631"  | "2, 41, 48"   | null
-        "Musee 6"   | "lundi au vendredi de 10h a 20h, samedi de 9h a 21h"  | "0578485612"  | null          | "4, 5"
-        "Musee 7"   | "lundi au vendredi de 10h a 22h, samedi de 9h a 0h"   | "0597521824"  | null          | null
+        unAccesMetro  | unAccesBus
+        "13, 17"      | "1, 2"
+        "21, 48, 78"  | ""
+        ""            | "3, 5"
+        ""            | ""
+        "2, 41, 48"   | null
+        null          | "4, 5"
+        null          | null
+
+    }
+
+    @Unroll
+    void "test l'invalidite d'un musee invalide"(String unNom, String desHoraires, String unTelephone){
+        given: "un musee initialise correctement"
+        Musee musee = new Musee(nom: unNom, horairesOuverture: desHoraires, telephone: unTelephone)
+
+        expect: "le musee est invalide"
+        musee.validate() == false
+
+        where:
+        unNom       | desHoraires                                           | unTelephone
+        "Musee"     | "lundi au vendredi de 10h a 20h"                      | ""
+        "Musee 2"   | "lundi au vendredi de 10h a 20h, samedi de 9h a 22h"  | null
+        "Musee 3"   | "lundi au vendredi de 10h a 21h"                      | "05487854"
+        "Musee 4"   | "lundi au vendredi de 12h a 20h, samedi de 10h a 22h" | "051521874556"
+        "Musee 5"   | ""                                                    | "0531285631"
+        "Musee 6"   | null                                                  | "0578485612"
+        ""          | "lundi au vendredi de 10h a 22h, samedi de 9h a 0h"   | "0597521824"
+        null        | "des horaires"                                        | "0575749416"
 
     }
 }
