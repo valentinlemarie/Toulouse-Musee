46d9aa2f979dcf5561186c3d775e05e9afed089b
valentinlemarie <valentin.lemarie@wanadoo.fr>
2015-04-18 18:16:51 +0200

    gestionaire

--- test/unit/musee/GestionnaireSpec.groovy
+++ test/unit/musee/GestionnaireSpec.groovy
@@ -2,6 +2,7 @@ package musee
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,5 +10,28 @@ import spock.lang.Specification
 @TestFor(Gestionnaire)
 class GestionnaireSpec extends Specification {
 
+    @Unroll
+    void "test la validite d'un gestionaire valide"(String nomNom){
+        given: "un gestionnaire initialise correctement"
+        Gestionnaire gestionnaire = new Gestionnaire(nom: nomNom)
+        expect: "un gestionnaire  valide"
+        gestionnaire.validate() == true
 
+        where:
+        nomNom
+        "valentin"
+    }
+
+    @Unroll
+    void "test la validite d'un gestionaire invalide"(String nomNom){
+        given: "un gestionnaire initialise correctement"
+        Gestionnaire gestionnaire = new Gestionnaire(nom: nomNom)
+        expect: "un gestionnaire  invalide"
+        gestionnaire.validate() == false
+
+        where:
+        nomNom
+        ""
+        null
+    }
 }
