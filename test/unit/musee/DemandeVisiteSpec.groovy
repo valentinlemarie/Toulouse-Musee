0f9e1fb044761865ac26059715b5ce582f615f08
valentinlemarie <valentin.lemarie@wanadoo.fr>
2015-04-18 16:06:15 +0200

    correction bug

--- test/unit/musee/DemandeVisiteSpec.groovy
+++ test/unit/musee/DemandeVisiteSpec.groovy
@@ -3,7 +3,7 @@ package musee
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 import spock.lang.Unroll
-
+// test commit
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
@@ -11,43 +11,28 @@ import spock.lang.Unroll
 class DemandeVisiteSpec extends Specification {
 
     @Unroll
-    void "test la validite d'une demande valide"(String unAccesMetro, String unAccesBus){
-        given: "une demande initialise correctement"
-        Musee musee = new Musee(nom: "Musee", horairesOuverture: "lundi au vendredi de 10h a 20h", telephone: "0563214582", accesMetro: unAccesMetro, accesBus: unAccesBus)
-
-        expect: "la demande est valide"
-        musee.validate() == true
+    void "test la validite d'une demande de visite valide"(String monCode, Date maDateDebutPeriode, Date maDateFinPeriode, int monNbPersonnes, boolean monStatus) {
+        given: "une demande de visite initialise correctement"
+        DemandeVisite demandeVisite = new DemandeVisite(code: monCode, dateDebutPeriode: maDateDebutPeriode, dateFinPeriode: maDateFinPeriode, status: monStatus)
+        expect: "une demande de visite  valide"
+        demandeVisite.validate() == true
 
         where:
-        unAccesMetro  | unAccesBus
-        "13, 17"      | "1, 2"
-        "21, 48, 78"  | ""
-        ""            | "3, 5"
-        ""            | ""
-        "2, 41, 48"   | null
-        null          | "4, 5"
-        null          | null
-
+        monCode | maDateDebutPeriode  | maDateFinPeriode    | monNbPersonnes | monStatus
+        "code"  | new Date(2014,03,25,10,15).parse('dd.MM.yyyy hh:mm', it) | new Date(2014,03,25,10,55).parse('dd.MM.yyyy hh:mm', it) | 1              | true
+        "code"  | new Date(2014,03,25,10,15).parse('dd.MM.yyyy hh:mm', it) | new Date(2014,03,25,10,15).parse('dd.MM.yyyy hh:mm', it) | 10             | false
     }
 
     @Unroll
-    void "test l'invalidite d'une demande invalide"(String unNom, String desHoraires, String unTelephone){
-        given: "une demande initialise sans code, dates, personnes ou statut"
-        Musee musee = new Musee(nom: unNom, horairesOuverture: desHoraires, telephone: unTelephone)
-
-        expect: "la demande est invalide"
-        musee.validate() == false
+    void "test l'invalidite d'une demande de visite invalide"(String monCode, Date maDateDebutPeriode, Date maDateFinPeriode, int monNbPersonnes, boolean monStatus) {
+        given: "une demande de visite initialise correctement"
+        DemandeVisite demandeVisite = new DemandeVisite(code: monCode, dateDebutPeriode: maDateDebutPeriode, dateFinPeriode: maDateFinPeriode, status: monStatus)
+        expect: "une demande de visite  invalide"
+        demandeVisite.validate() == false
 
         where:
-        unNom       | desHoraires                                           | unTelephone
-        "Musee"     | "lundi au vendredi de 10h a 20h"                      | ""
-        "Musee 2"   | "lundi au vendredi de 10h a 20h, samedi de 9h a 22h"  | null
-        "Musee 3"   | "lundi au vendredi de 10h a 21h"                      | "05487854"
-        "Musee 4"   | "lundi au vendredi de 12h a 20h, samedi de 10h a 22h" | "051521874556"
-        "Musee 5"   | ""                                                    | "0531285631"
-        "Musee 6"   | null                                                  | "0578485612"
-        ""          | "lundi au vendredi de 10h a 22h, samedi de 9h a 0h"   | "0597521824"
-        null        | "des horaires"                                        | "0575749416"
-
+        monCode | maDateDebutPeriode  | maDateFinPeriode            | monNbPersonnes | monStatus
+        ""      | new Date(2014,03,25,10,15).parse('dd.yyyy hh:mm', it)          | new Date(2014,023,25,10,30).parse('d.M.yyy hh:m', it)       | 0             | true
+        null    | new Date(2014,03,25,10,15).parse('dd.MM.yyy hh:m', it)       | new Date(2014,03,250,100,155).parse('dd.MM.yyy hh:m', it)        | -10             | false
     }
 }
