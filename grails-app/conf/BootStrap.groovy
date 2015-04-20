6cbe06eed4931bdfb046f8c9d3ee2a1bb35c560f
valentinlemarie <valentin.lemarie@wanadoo.fr>
2015-04-20 18:46:13 +0200

    controleur affichage musee

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,16 @@
+import musee.Adresse
+import musee.AdresseService
+import musee.GestionaireService
+import musee.Gestionnaire
+
 class BootStrap {
 
+    AdresseService adresseService
+    GestionaireService gestionaireService
+
     def init = { servletContext ->
+        adresseService.insertOrUpdateAdresse( new Adresse(rue: "rue de Rudel",numero: 26,codePostal: "81000",ville: "Albi")  )
+        gestionaireService.insertOrUpdateGestionnaire(new Gestionnaire(nom: "ToTo"))
     }
     def destroy = {
     }
--- grails-app/services/musee/GestionaireService.groovy
+++ grails-app/services/musee/GestionaireService.groovy
@@ -4,24 +4,13 @@ import grails.transaction.Transactional
 
 @Transactional
 class GestionaireService {
-    Gestionnaire gestionnaire
-    Musee musee
-    def serviceMethod() {
 
-    }
 
-    def insertOrGestionnaire(Gestionnaire gestionnaire1,Musee musee1){
+    Gestionnaire insertOrUpdateGestionnaire(Gestionnaire gestionnaire1){
         gestionnaire1.save()
-        gestionnaire1.addToMusees(musee1)
-        musee1.save()
-        gestionnaire1
     }
 
-    def removeGestionaire(Gestionnaire gestionnaire1,Gestionnaire gestionnaire2){
-
-        gestionnaire2.addToMusees(gestionnaire1.getMusees())
-        gestionnaire1.getMusees().remove();
-        gestionnaire1
-
+    def deleteGestionaire(Gestionnaire gestionnaire1){
+        gestionnaire1.delete()
     }
 }
