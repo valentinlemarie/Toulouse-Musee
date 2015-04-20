f5a9cb1db49714de1e1fae78ab006836ef0338e2
valentinlemarie <valentin.lemarie@wanadoo.fr>
2015-04-20 14:50:58 +0200

    mise a jour musee service

--- grails-app/services/musee/MuseeService.groovy
+++ grails-app/services/musee/MuseeService.groovy
@@ -6,9 +6,8 @@ import grails.transaction.Transactional
 class MuseeService {
 
     Musee insertOrUpdateMusee(Musee musee, Adresse adresse1, Gestionnaire gestionnaire1){
-        musee.Adresse = adresse1
-        gestionnaire1.save()
-        gestionnaire1.addToMusees(musee)
+        musee.adresse = adresse1
+        musee.gestionnaire = gestionnaire1
         musee.save()
     }
 
